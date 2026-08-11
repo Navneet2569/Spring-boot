58f596f00e6c28e50b6509408f8374a28fbc5407
Navneet Singh <navneetprod@MngEnvMCAP178253.onmicrosoft.com>
2026-08-11 22:53:03 +0530

    Learned Proxies, Reflection, Cacheable and CGLIB in Java
    Learned Proxies, Reflection, Cacheable and CGLIB in Java

--- app/src/main/groovy/org/example/service/OrderService.java
+++ app/src/main/groovy/org/example/service/OrderService.java
@@ -2,7 +2,6 @@ import java.beans.JavaBean;
 
 import org.example.service.*;
 
-@JavaBean
 public class OrderService {
     public static OrderService createInstance(UserService userService, UserConfig userConfig) {
         return null;
