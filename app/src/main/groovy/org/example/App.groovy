abd8578f57c1bd3a8a73a21a22bcc9036190fcf1
Navneet Singh <116500700+Navneet2569@users.noreply.github.com>
2026-08-18 22:06:56 +0530

    Learned CAP Theorem and choosing databases

--- app/src/main/groovy/org/example/App.groovy
+++ app/src/main/groovy/org/example/App.groovy
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class App {
 
     //Learned Authentication and Authorization
-
+    //Learned CAP Theorem and choosing databases
     static void main(String[] args) {
         println "Starting application"
         SpringApplication.run(App, args)
